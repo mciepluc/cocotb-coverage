89e199ec7c6f9afe1b025fa4c68d1e067b4cf48e
Marek <mciepluc@gmail.com>
2023-11-15 18:53:19 +0100

    deprecate use of TestFailure

--- tests/test_crv/crv_test_main.py
+++ tests/test_crv/crv_test_main.py
@@ -3,11 +3,9 @@ import cocotb
 import pytest
 import crv_test
 from cocotb.triggers import Timer
-from cocotb.result import TestFailure
 
 @cocotb.test()
 def test_crv(dut):
     exitcode = pytest.main()
-    if exitcode != pytest.ExitCode.OK:
-        raise TestFailure()
+    assert exitcode == pytest.ExitCode.OK
     yield Timer(1000)
